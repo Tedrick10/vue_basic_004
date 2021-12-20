b6bd40092c705da2bdc9267e3a0d1a2ad4e8ab0e
Thet Tun Kyaw <thettunkyaw301098@gmail.com>
2021-12-20 17:06:04 +0630

    Updated v-show and v-for.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const app = Vue.createApp({
   data() {
-    return { enteredGoalValue: "", goals: [] };
+    return {
+      enteredGoalValue: "",
+      goals: [],
+      information: { name: "Thet Tun Kyaw", age: "24", birthPlace: "Mandalay" },
+    };
   },
   methods: {
     enteredGoal() {
       this.goals.push(this.enteredGoalValue);
       this.enteredGoalValue = "";
+    },
+    removedGoal(index) {
+      this.goals.splice(index, 1);
     },
   },
 });
